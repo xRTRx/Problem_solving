ada5b509896438c36ea5d03144e344ad207c21c9
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-12 23:38:19 +0500

    Pig_Latin

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-# This is a sample Python script.
+def pig_latin(s):
+    i = 0
+    result = str()
+    temp = str()
+    while i != len(s):
+        temp = temp + s[i]
+        if s[i] == ' ' or (len(s) - 1 == i):
+            fl = temp[0]  # first letter
+            temp = temp.replace(temp[0], '', 1)
+            temp = temp.strip() + fl + "ay "
+            result = result + temp
+            temp = ""
+        i = i + 1
+    return result
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print("Hello world")
+
+if __name__ == '__main__':
+    print(pig_latin("nevermind youve got them"))
+    print(pig_latin("go over there"))
+    print(pig_latin("sally knows best"))
+    print(pig_latin("nevermind"))
