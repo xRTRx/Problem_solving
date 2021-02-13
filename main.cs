b99d40a01bf65720f4b84550355249ddea4894b3
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-13 22:46:34 +0500

    Deja Vu

--- main.cs
+++ main.cs
@@ -1,11 +1,29 @@
-using System; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Application { 
-	
-	class Program { 
-		static void Main(string[] args) { 
-			Console.WriteLine("Hello World!"); 
-		} 
-	} 
-} 
- 
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            string s = Console.ReadLine();
+            Dictionary<char, Int16> letters = new Dictionary<char, Int16>();
+            bool check = false;
+            foreach(var i in s) {
+                if (letters.ContainsKey(i)) {
+                    check = true;
+                    letters[i] += 1;
+                    Console.WriteLine("Deja Vu");
+                    break;
+                } else {
+                    letters.Add(i, 1);
+                }
+            }
+            if(check == false) {
+                Console.WriteLine("Unique");
+            }
+
+        }
+    }
+}
