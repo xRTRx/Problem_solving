d364eb4b25f7d597ff4086c346fbf2741f77b7eb
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-13 23:40:04 +0500

    The Spy Life

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-    cout << "Hello world";
+    string s;
+	getline(cin, s); //spaces aren't ignoring
+	//removing special characters and numbers
+  	s.erase(remove_if(s.begin(), s.end(), [](char c){
+  	  		if (c == ' ') { return false; }
+  	  		else { return !isalpha(c);	  }  		}), s.end());
+  	reverse(s.begin(), s.end());
+  	cout << s;
     return 0;
 }
