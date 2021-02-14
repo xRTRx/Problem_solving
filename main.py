64f514175cc9acb1b35649163092f29ab511f970
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-14 20:50:59 +0500

    Symbols

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-def pig_latin(s):
-    i = 0
-    result = str()
-    temp = str()
-    while i != len(s):
-        temp = temp + s[i]
-        if s[i] == ' ' or (len(s) - 1 == i):
-            fl = temp[0]  # first letter
-            temp = temp.replace(temp[0], '', 1)
-            temp = temp.strip() + fl + "ay "
-            result = result + temp
-            temp = ""
-        i = i + 1
-    return result
-
-
+import re
 
 if __name__ == '__main__':
-    print(pig_latin("nevermind youve got them"))
-    print(pig_latin("go over there"))
-    print(pig_latin("sally knows best"))
-    print(pig_latin("nevermind"))
+    # string = input()
+    string = "H#i w@hat7s yo0u&r n@a$me?"
+    string = re.sub('[^a-zA-Z0-9 \n]', '', string)
+    print(string)
+
