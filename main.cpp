2d6658a2f7cd2ce579772788b94844d1631484dd
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-14 20:55:04 +0500

    Symbols

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@ using namespace std;
 int main(int argc, char *argv[]){
     string s;
 	getline(cin, s); //spaces aren't ignoring
-	//removing special characters and numbers
+	//removing special characters
   	s.erase(remove_if(s.begin(), s.end(), [](char c){
   	  		if (c == ' ') { return false; }
-  	  		else { return !isalpha(c);	  }  		}), s.end());
-  	reverse(s.begin(), s.end());
+  	  		else { return !isalnum(c);	  }  		}), s.end());
   	cout << s;
     return 0;
 }
