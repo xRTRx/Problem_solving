d2764f2b73a25ad7af73f048e3d221a3f2c9b8ff
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-16 21:23:30 +0500

    Average Word Length

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import re
+import math
+
 if __name__ == '__main__':
-    # string = input()
-    string = "I have 2 apples and 4 bananas"
-    for i in range(10, 0, -1):
-        string = string.replace(str(i), ['zero', 'one', 'two', 'three', 'four', 'five',
-                                         'six', 'seven', 'eight', 'nine', 'ten'][i])
-    print(string)
+    string = input()
+    # string = "the longest word in the dictionary is..."  #7w avg = 4.42 -> 5
+    string = re.sub('[^a-zA-Z0-9 \n]', '', string).split()
+    wc = 0  # word count
+    for i in string:
+        wc += len(i)
+    print(math.ceil(wc / len(wl)))
