c731a5496c99f944ad2ee36c4486b53476f218e1
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-17 21:58:08 +0500

    YouTube Link Finder

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-    string s("Hello 2 World");
-    string letters = {"abcdefghijklmnopqrstuvwxyz"};
-    for (size_t i = 0; i < s.size(); i++){
-        s[i] = tolower(s[i]);
-        if (s[i] == ' ' || !isalpha(s[i])) // skipping spaces and numbers
-            continue;
-        else
-            s.replace(i, 1, string(1, letters[25 - letters.find(s[i])]));
-    }
-  cout << s;
-    return 0;
+string find_link(const string& s){
+  string link;
+  if (s.find("?v=") != string::npos){
+	link = s.substr(1 + s.find_last_of('='));
+  } else {
+	link = s.substr(1 + s.find_last_of('/'));
+  }
+  return link;
+}
+
+int main(int argc, char *argv[]) {
+  string s1("https://www.youtube.com/watch?v=kbxkq_w51PM");
+  string s2("https://youtu.be/KMBBjzp5hdc");
+  cout << find_link(s1) << endl;
+  cout << find_link(s2);
+  return 0;
 }
