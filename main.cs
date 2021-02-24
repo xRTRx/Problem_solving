b488f3c0836f2a12421dde05645e86c49e1e44c7
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-24 22:24:14 +0500

    Password validation

--- main.cs
+++ main.cs
@@ -7,34 +7,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1 {
     class Program {
-        static string convert_time(string s) {
-            string result = null;
-            int hours = int.Parse(s.Substring(0, s.IndexOf(':')));
-            if ((s.Contains("AM") && (hours != 12)) || (s.Contains("PM") && (hours == 12))) {
-                if (hours > 10) {
-                    result = hours + s.Substring(s.IndexOf(':'), 3);
+        static string pcheck(string password) {
+            int cdigits = 0, cschars = 0; // counters
+            foreach(char i in password) {
+                if (Char.IsDigit(i)) {
+                    ++cdigits;
+                } else if(!Char.IsLetterOrDigit(i)) {
+                    ++cschars;
                 } else {
-                    result = Convert.ToString('0') + hours + s.Substring(s.IndexOf(':'), 3);
+                    continue;
                 }
-            } else if (s.Contains("AM") && hours == 12) {
-                result = "00" + s.Substring(s.IndexOf(':'), 3);
-            } else if (s.Contains("PM") && hours != 12) {
-                result = Convert.ToString(hours + 12) + s.Substring(s.IndexOf(':'), 3);
             }
-            return result;
+            if(cdigits >= 2 && cschars >= 2 && password.Length >= 7) {
+                return "Strong";
+            } else {
+                return "Weak";
+            }
         }
+
         static void Main(string[] args) {
-            //string input = Console.ReadLine();
-            //Console.WriteLine(convert_time(input));
-            Console.WriteLine(convert_time("0:50AM"));// 00:50
-            Console.WriteLine(convert_time("3:00AM"));// 03:00
-            Console.WriteLine(convert_time("9:00AM"));// 09:00
-            Console.WriteLine(convert_time("11:00AM"));// 11:00
-            Console.WriteLine(convert_time("12:00AM"));// 00:00
-            Console.WriteLine(convert_time("12:40PM"));// 12:40
-            Console.WriteLine(convert_time("7:00PM"));// 19:00
-            Console.WriteLine(convert_time("11:00PM"));// 23:00
-            Console.WriteLine(convert_time("00:15PM"));// 12:15
+            string password = Console.ReadLine();
+            Console.WriteLine(pcheck("123qwe##rt")); // Strong
+            Console.WriteLine(pcheck("1qwerty*")); // Weak
         }
     }
 }
