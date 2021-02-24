bdf48997bbd0d3c65da44f990dc0d5132b01c3cc
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-02-24 22:21:52 +0500

    Security

--- main.cpp
+++ main.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-string find_link(const string& s){
-  string link;
-  if (s.find("?v=") != string::npos){
-	link = s.substr(1 + s.find_last_of('='));
-  } else {
-	link = s.substr(1 + s.find_last_of('/'));
+bool alarm(const string& s) {
+  size_t money = s.find('$');
+  size_t thief = s.find('T');
+  vector<size_t> guards;
+  for (size_t i = 0; i < s.size(); i++) {
+	if (s[i] == 'G')
+	  guards.push_back(i);
   }
-  return link;
+  bool r = false;
+  for (const auto& g : guards) {
+	if (money > g && g < thief)
+	  r = true;
+	else if (money < g && g > thief)
+	  r = true;
+	if (money < g && g < thief)
+	  r = false;
+	else if (money > g && g > thief)
+	  r = false;
+  }
+  return r;
 }
 
 int main(int argc, char *argv[]) {
-  string s1("https://www.youtube.com/watch?v=kbxkq_w51PM");
-  string s2("https://youtu.be/KMBBjzp5hdc");
-  cout << find_link(s1) << endl;
-  cout << find_link(s2);
+//  string input;
+//  getline(cin, input);
+  string s1 = "xxxGxx$xxxT"; //1
+  string s2 = "xGxxxTxx$";   //1
+  string s3 = "xGxxTx$xxG";  //1
+  string s4 = "xx$xTxxGxx";  //1
+  cout << alarm(s1) << endl;
+  cout << alarm(s2) << endl;
+  cout << alarm(s3) << endl;
+  cout << alarm(s4) << endl;
+  cout << alarm("x$xxGxxxT") << endl; //0
+  cout << alarm("xGGx$xGxT") << endl; //0
   return 0;
 }
