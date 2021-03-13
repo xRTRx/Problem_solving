ee96f56c0502738181d25ab46d888351f7fd5edc
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-03-13 23:00:09 +0500

    Shooting Game

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
+class Enemy:
+    name = ""
+    lives = 0
+
+    def __init__(self, name, lives):
+        self.name = name
+        self.lives = lives
+
+    def hit(self):
+        self.lives -= 1
+        if self.lives <= 0:
+            print(self.name + ' killed')
+        else:
+            print(self.name + ' has ' + str(self.lives) + ' lives')
+
+
+class Monster(Enemy):
+    def __init__(self):
+        super().__init__('Monster', 3)
+
+
+class Alien(Enemy):
+    def __init__(self):
+        super().__init__('Alien', 5)
+
 
 if __name__ == '__main__':
-    txt = input()
-    dict = {}
-    for i in text:
-        if i in dict:
-            dict[i] += 1
+    m = Monster()
+    a = Alien()
+
+    while True:
+        x = input()
+        if x == 'exit':
+            break
+        elif x == 'laser':
+            a.hit()
+        elif x == 'gun':
+            m.hit()
         else:
-            dict[i] = 1
-    print(dict)
+            continue
