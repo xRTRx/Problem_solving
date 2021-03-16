43b7606b41d1b17e27beb1d8124d9f02ee29e1b5
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-03-16 20:18:24 +0500

    Title Encoder + File writer

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-def decor(func):
-    def wrap(arr, x):
-        r = func(arr, x)
-        print(r[0])
-
-    return wrap
-
-
-@decor
-def binary_search(arr, x):
-    low = 0
-    high = len(arr) - 1
-    o = 0
-    while low <= high:
-        o += 1
-        mid = (high + low) // 2
-        if arr[mid] < x:
-            low = mid + 1
-        elif arr[mid] > x:
-            high = mid - 1
-        else:
-            return mid, o
-    return -1, o
-
-
 if __name__ == '__main__':
-    binary_search([0, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 19], 9)
+    with open("books.txt", "a") as f:
+        while True:
+            i = input()
+            if i == "stop":
+                break
+            f.write(i + '\n')
+
+    with open("books.txt", "r") as f:
+        for i in f:
+            for x in i.split():
+                print(x[0], end='')
+            print()
