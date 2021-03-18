abeabb30ce46c4b3aeb244e032164cc67d5cb8b2
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-03-18 22:33:31 +0500

    Revenue Growth Analysis

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-if __name__ == '__main__':
-    with open("books.txt", "a") as f:
-        while True:
-            i = input()
-            if i == "stop":
-                break
-            f.write(i + '\n')
+data = {
+    "100-90": 25, "42-01": 48, "55-09": 12, "128-64": 71, "002-22": 18, "321-54": 19, "097-32": 33, "065-135": 64,
+    "99-043": 80, "111-99": 11, "123-019": 5, "109-890": 72, "132-123": 27, "32-908": 27, "008-09": 25,
+    "055-967": 35, "897-99": 44, "890-98": 56, "344-32": 65, "43-955": 59, "001-233": 9, "089-111": 15,
+    "090-090": 17, "56-777": 23, "44-909": 27, "13-111": 21, "87-432": 15, "87-433": 14, "87-434": 23, "87-435": 11,
+    "87-436": 12, "87-437": 16, "94-121": 15, "94-122": 35, "80-089": 10, "87-456": 8, "87-430": 40
+}
 
-    with open("books.txt", "r") as f:
-        for i in f:
-            for x in i.split():
-                print(x[0], end='')
-            print()
+if __name__ == '__main__':
+    age = int(input())
+    total_new, total_original = 0, 0
+    for i in data.values():
+        if i < 18:
+            total_original += 5
+        else:
+            total_original += 20
+        if i < age:
+            total_new += 5
+        else:
+            total_new += 20
+    print(int((total_new-total_original)/total_original*100))
