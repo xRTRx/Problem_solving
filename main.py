852ca78680f8e5e9d9649acd0719926bf9387d2b
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-03-23 21:20:03 +0500

    Basketball Players

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-data = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3]
-    mean = sum(data) / len(data)
-    variance = sum((i - mean) ** 2 for i in data) / len(data)
-    print(variance)  # variance - дисперсия
+import math
+
+if __name__ == '__main__':
+    players = [180, 172, 178, 185, 190, 195, 192, 200, 210, 190]
+    mean = sum(players) / len(players)
+    variance = sum((i - mean) ** 2 for i in players) / len(players)
+    # standard deviation = √variance
+    sd = math.sqrt(variance)
+    count = 0
+    for i in players:
+        if mean + sd > i > mean - sd:  # (199 178)
+            count += 1
+    print(count)
