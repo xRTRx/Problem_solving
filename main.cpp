b0f1cdc67acf9eb475d429dd62fb65aa2ca579ac
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-07-12 22:29:19 +0500

    Isomorphic Strings

--- main.cpp
+++ main.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
-#include <vector>
-
+#include <map>
+#include <string>
 using namespace std;
 
-bool alarm(const string& s) {
-  size_t money = s.find('$');
-  size_t thief = s.find('T');
-  vector<size_t> guards;
-  for (size_t i = 0; i < s.size(); i++) {
-	if (s[i] == 'G')
-	  guards.push_back(i);
+class Solution {
+ public:
+  bool isIsomorphic(const string &s, const string &t) {
+    for (int i = 0; i < s.size(); ++i) {
+      ms.emplace(s[i], t[i]);
+      mt.emplace(t[i], s[i]);
+    }
+    for (int i = 0; i < s.size(); ++i) {
+      if (t[i] != ms.at(s[i]) || s[i] != mt.at(t[i])) return false;
+    }
+    return true;
   }
-  bool r = false;
-  for (const auto& g : guards) {
-	if (money > g && g < thief)
-	  r = true;
-	else if (money < g && g > thief)
-	  r = true;
-	if (money < g && g < thief)
-	  r = false;
-	else if (money > g && g > thief)
-	  r = false;
-  }
-  return r;
-}
 
-int main(int argc, char *argv[]) {
-//  string input;
-//  getline(cin, input);
-  string s1 = "xxxGxx$xxxT"; //1
-  string s2 = "xGxxxTxx$";   //1
-  string s3 = "xGxxTx$xxG";  //1
-  string s4 = "xx$xTxxGxx";  //1
-  cout << alarm(s1) << endl;
-  cout << alarm(s2) << endl;
-  cout << alarm(s3) << endl;
-  cout << alarm(s4) << endl;
-  cout << alarm("x$xxGxxxT") << endl; //0
-  cout << alarm("xGGx$xGxT") << endl; //0
+ private:
+  map<char, char> ms, mt;
+};
+
+int main() {
+  Solution s;
+  cout << s.isIsomorphic("badc", "baba");
+  //  cout << s.isIsomorphic("bar", "foo");
+  //  cout << s.isIsomorphic("paper", "title");
+  //  cout << s.isIsomorphic("egg", "add");
   return 0;
 }
