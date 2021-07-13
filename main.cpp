e91cf8f75d5d542f25d21674e2bce1259f67c8eb
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-07-13 16:18:17 +0500

    Exceptional Server

--- main.cpp
+++ main.cpp
@@ -1,52 +1,51 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <cmath>
 #include <exception>
+#include <stdexcept>
 using namespace std;
 
-/* Define the exception here */
-class BadLengthException : public exception {
- public:
-  explicit BadLengthException(int n){
-    m[0] = '0' + n;
-    m[1] = '\0';
-  }
-  virtual const char* what() const noexcept{
-    return m;
-  }
+class Server {
  private:
-  char m[2];
+  static int load;
+ public:
+  static int compute(long long A, long long B) {
+	load += 1;
+	if (A < 0) {
+	  throw std::invalid_argument("A is negative");
+	}
+	vector<int> v(A, 0);
+	int real = -1, cmplx = sqrt(-1);
+	if (B == 0) throw 0;
+	real = (A / B) * real;
+	int ans = v.at(B);
+	return real + A - B * ans;
+  }
+  static int getLoad() {
+	return load;
+  }
 };
-
-
-bool checkUsername(string username) {
-	bool isValid = true;
-	int n = username.length();
-	if(n < 5) {
-		throw BadLengthException(n);
-	}
-	for(int i = 0; i < n-1; i++) {
-		if(username[i] == 'w' && username[i+1] == 'w') {
-			isValid = false;
-		}
-	}
-	return isValid;
-}
+int Server::load = 0;
 
 int main() {
-	int T; cin >> T;
-	while(T--) {
-		string username;
-		cin >> username;
-		try {
-			bool isValid = checkUsername(username);
-			if(isValid) {
-				cout << "Valid" << '\n';
-			} else {
-				cout << "Invalid" << '\n';
-			}
-		} catch (BadLengthException e) {
-			cout << "Too short: " << e.what() << '\n';
-		}
+  int T;
+  cin >> T;
+  while (T--) {
+	long long A, B;
+	cin >> A >> B;
+	try {
+	  cout << Server::compute(A, B) << endl;
 	}
-	return 0;
+	catch (bad_alloc &e) {
+	  cout << "Not enough memory" << endl;
+	}
+	catch (exception &e) {
+	  cout << "Exception: " << e.what() << endl;
+	}
+	catch (...) {
+	  cout << "Other Exception" << endl;
+	}
+  }
+  cout << Server::getLoad() << endl;
+  return 0;
 }
