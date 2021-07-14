0a94fb0664e1047ef851ebba4a6f3a8738775b9e
Artur_Shakirov <ai.shakirov1@gmail.com>
2021-07-14 11:03:06 +0500

    Add Two Numbers

--- main.cpp
+++ main.cpp
@@ -1,51 +1,46 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <exception>
-#include <stdexcept>
 using namespace std;
 
-class Server {
- private:
-  static int load;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
  public:
-  static int compute(long long A, long long B) {
-	load += 1;
-	if (A < 0) {
-	  throw std::invalid_argument("A is negative");
+  static ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    int sum = 0;
+    ListNode *r1;
+    ListNode **r2 = &r1;
+	while (l1 != nullptr || l2 != nullptr || sum > 0){
+	  if (l1 != nullptr){
+	    sum += l1->val;
+	    l1 = l1->next;
+	  }
+	  if (l2 != nullptr){
+	    sum += l2->val;
+	    l2 = l2->next;
+	  }
+	  *r2 = new ListNode(sum%10);
+	  sum /= 10;
+	  r2 = &((*r2)->next);
 	}
-	vector<int> v(A, 0);
-	int real = -1, cmplx = sqrt(-1);
-	if (B == 0) throw 0;
-	real = (A / B) * real;
-	int ans = v.at(B);
-	return real + A - B * ans;
-  }
-  static int getLoad() {
-	return load;
+	return r1;
   }
 };
-int Server::load = 0;
 
 int main() {
-  int T;
-  cin >> T;
-  while (T--) {
-	long long A, B;
-	cin >> A >> B;
-	try {
-	  cout << Server::compute(A, B) << endl;
-	}
-	catch (bad_alloc &e) {
-	  cout << "Not enough memory" << endl;
-	}
-	catch (exception &e) {
-	  cout << "Exception: " << e.what() << endl;
-	}
-	catch (...) {
-	  cout << "Other Exception" << endl;
-	}
+  ListNode l1(2), l2(4, &l1), l3(3, &l2); // l3->l2->l1
+  ListNode n1(5), n2(6, &n1), n3(4, &n2); // n3->n2->n1
+//  ListNode l1(2), l2(4, &l1), l3(3, &l2), l4(1, &l3);
+//  ListNode n1(5), n2(6, &n1), n3(4, &n2);
+  ListNode *t = Solution::addTwoNumbers(&l3, &n3);
+  while (t != nullptr){
+    cout << t->val << " ";
+    t = t->next;
   }
-  cout << Server::getLoad() << endl;
   return 0;
 }
